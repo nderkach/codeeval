d1b527b027dd4199ec6d5554525947b015254e62
Nikolay Derkach <nderk@me.com>
2013-10-30 20:44:44 +0100

    milo.py add unit tests header

--- milo.py
+++ milo.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python
+"""Sample code to read in test cases:
+
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    # 'test' represents the test case, do something with it
+    # ...
+    # ...
+
+test_cases.close()
+"""
+
 import sys, itertools
 from fractions import gcd
 import collections
