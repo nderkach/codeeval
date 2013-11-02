7f8c7597104b7241ca74b0bc8e19c92d9030d934
Nikolay Derkach <nderk@me.com>
2013-11-02 21:32:30 +0100

    Bridges problem

---
+++ bridges.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import sys
+from collections import namedtuple, defaultdict
+from itertools import product
+
+def if_intersect(tup1, tup2):
+    """ Check if two lines intersect
+        Input: 2 tuples ([x1, y1], [x2, y2]) and ([x3, y3], [x4, y4])
+
+        >>> if_intersect( ([1, 1], [3, 3]), ([2, 1], [1, 3]) )
+        True
+        >>> if_intersect( ([1, 1], [3, 3]), ([2, 1], [3, 2]) )
+        False
+        
+    """
+    Point = namedtuple('Point', 'x, y')
+    p1a = Point(*tup1[0])
+    p1b = Point(*tup1[1])
+    p2a = Point(*tup2[0])
+    p2b = Point(*tup2[1])
+
+    if max(p1a.x, p1b.x) < min(p2a.x, p2b.x):
+        return False
+
+    a1 = (p1b.y-p1a.y)/(p1b.x-p1a.x)
+    a2 = (p2b.y-p2a.y)/(p2b.x-p2a.x)
+    b1 = p1a.y - a1*p1a.x
+    b2 = p2a.y - a2*p2a.x
+
+    if (a1 == a2):
+        return False
+
+    crosspoint = (b2-b1)/(a1-a2)
+
+    if ((crosspoint < max(min(p1a.x, p1b.x), min(p2a.x, p2b.x))) or (crosspoint > min(max(p1a.x, p1b.x), max(p2a.x, p2b.x)))):
+        return False
+    else:
+        return True
+
+def get_no_of_intersections(bridges):
+    no_of_intersections = defaultdict(list)
+    for pr in product(bridges.keys(), repeat=2):
+        if if_intersect(*pr):
+            no_of_intersections[bridges[pr[0]]].append(bridges[pr[1]])
+    return no_of_intersections
+
+def build_bridges(bridges):
+    """ Build all the bridges starting with those that have no no_of_intersections
+        and then incresing the number of those
+    """
+    built_bridges = set()
+    no_of_intersections = get_no_of_intersections(bridges)
+
+    for bridgeid in bridges.values():
+        if bridgeid not in no_of_intersections.keys():
+            built_bridges.add(bridgeid)
+    
+    for bridgeid, intersected_bridges in sorted(no_of_intersections.items(), key=lambda x: len(x[1])):
+        if not any([bridge for bridge in intersected_bridges if bridge in built_bridges]):
+            built_bridges.add(bridgeid)
+
+    for bridge in sorted(built_bridges):
+        print(bridge)
+
+if __name__=="__main__":
+    bridges = {}
+    with open(sys.argv[1]) as f:
+        for line in f:
+            bid, tup = line.strip().split(": ")
+            bridges[tuple([tuple(c) for c in eval(tup)])] = int(bid)
+
+    build_bridges(bridges)
+
+    sys.exit(0)
--- uglynum.py
+++ uglynum.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, itertools
+import sys, itertools, re
 
 def is_ugly(num):
     """Is it an ugly number
@@ -18,33 +18,51 @@ def is_ugly(num):
 
     return any([not num%i for i in [2, 3, 5, 7]])
 
-def count_ugly(num):
+def count_ugly_numbers(num):
     """Counts the number of ugly numbers which could be derived from num
 
+        >>> count_ugly_numbers('1')
+        0
+        >>> count_ugly_numbers('9')
+        1
+        >>> count_ugly_numbers('011')
+        6
+        >>> count_ugly_numbers('12345')
+        64
     """
 
     cnt = 0
-    # # start by checking the number itself
-    # cnt = int(is_ugly(num))
-    snum = num
 
-    sign_list = [c for c in itertools.product(['+', '-', ''], repeat=len(snum)-1)]
+    # get the list of permutations of '+', '-' and ''(no sign) signs
+    sign_list = [sign_permutation for sign_permutation in itertools.product(['+', '-', ''], repeat=len(num)-1)]
 
-    for s in [list(t) for t in sign_list]:
+    """
+       interleave the number with all the sign permutations
+       3**(D-1) permutations, where D is the number of digits in num
+       i.e. if num = 123
+       the outer loop will produce string like:
+       123
+       1+2+3
+       12+3
+       1+23
+       1+2-3
+       1-2+3
+       1-2-3
+       1-23
+       12-3
+    """
+    for s in [list(tup) for tup in sign_list]:
         l = ""
-        for d in range(len(snum)-1):
-            l+=(snum[d] + s[d])
-        l+=snum[-1]
-        # print s
-        # print l
-        # print eval(l.lstrip('0'))
-        cnt += is_ugly(eval(l.lstrip('0')))
+        for d in range(len(num)-1):
+            l += (num[d] + s[d])
+        l += num[-1]
+        # remove leading zeros in operands, as eval treats such numbers as octal
+        cnt += is_ugly(eval(re.sub(r'\b0+(?!\b)', '', l)))
 
     return cnt
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         for line in f:
-            print line.strip()
-            print count_ugly(line.strip())
-            print '------'
+            print count_ugly_numbers(line.strip())
+    sys.exit(0)
